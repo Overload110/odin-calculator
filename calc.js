f2894b2ed2d7f0862a1ba8b1ceebc05f914629d6
Overload110 <149197288+Overload110@users.noreply.github.com>
2023-11-14 13:12:36 -0600

    Add eventlistener

--- calc.js
+++ calc.js
@@ -1,17 +1,33 @@
+let firstNum = 0;
+let secondNum = 0;
+let operation = "";
+let displayVal = "";
+let btns = document.querySelectorAll('input');
+let display = document.getElementById("display");
+
 function operate(operation, a, b){
     switch(operation){
         case "+":
             return a + b;
-        break;
+            break;
         case "-":
             return a - b;
-        break;
+            break;
         case "*":
             return a * b;
-        break;
+            break;
         case "÷":
             return a / b;
-        break;
     }
 };
 
+for(let i of btns){
+    i.addEventListener("click", function() {
+        if(i.className == "number"){
+            displayVal += i.value;
+            display.innerHTML = displayVal;
+        }else{
+            
+        }
+    });
+};
