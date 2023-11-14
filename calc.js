f4cf1df5a740744686bc91468b694459e64686c0
Overload110 <149197288+Overload110@users.noreply.github.com>
2023-11-14 13:38:37 -0600

    Finish basic calculator
    Clear not working.

--- calc.js
+++ calc.js
@@ -1,7 +1,7 @@
 let firstNum = 0;
 let secondNum = 0;
 let operation = "";
-let displayVal = "";
+let displayVal = null;
 let btns = document.querySelectorAll('input');
 let display = document.getElementById("display");
 
@@ -24,10 +24,30 @@ function operate(operation, a, b){
 for(let i of btns){
     i.addEventListener("click", function() {
         if(i.className == "number"){
-            displayVal += i.value;
+            (displayVal == null) ?  displayVal = i.value: displayVal += i.value;
             display.innerHTML = displayVal;
-        }else{
-            
+        }else if (i.value == "clear"){
+            displayVal = null;
+            display.innerHtml = null;
+            operation = "";
+            firstNum = 0;
+            secondNum = 0;
+        }else if (i.value == "="){
+            secondNum = displayVal;
+            console.log('idk');
+            displayVal = operate(operation, firstNum, secondNum);
+            firstNum = displayVal;
+            display.innerHTML = displayVal;
+        }else if (operation == ""){
+            setOperation(i.value);
         }
     });
 };
+
+function setOperation(sign){
+    operation = sign;
+    firstNum = +displayVal;
+    displayVal = null;
+    display.innerHTML = displayVal;
+    console.log("working");
+}
