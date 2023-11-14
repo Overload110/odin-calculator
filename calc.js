c086a857f7b4e9f83841618649a34c9110b33016
Overload110 <149197288+Overload110@users.noreply.github.com>
2023-11-14 11:59:30 -0600

    Create operate function

---
+++ calc.js
@@ -0,0 +1,17 @@
+function operate(operation, a, b){
+    switch(operation){
+        case "+":
+            return a + b;
+        break;
+        case "-":
+            return a - b;
+        break;
+        case "*":
+            return a * b;
+        break;
+        case "÷":
+            return a / b;
+        break;
+    }
+};
+
